235c697fa148668afcef0ab0005ea032377e9744
parkchanjong <7000cj@gmail.com>
2021-03-22 23:41:42 +0900

    MaxOfArray

---
+++ MaxOfArray/MaxOfArray.java
@@ -0,0 +1,31 @@
+package MaxOfArray;
+
+import java.util.*;
+
+public class MaxOfArray {
+    static int maxOf(int[] a) {
+        int max = a[0];
+
+        for (int i = 0; i < a.length; i++)
+            if (a[i] > max)
+                max = a[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("키의 최대값을 구합니다.");
+        System.out.println("사람 수 : ");
+        int num = sc.nextInt();
+
+        int[] height = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("height[" + i + "] : ");
+            height[i] = sc.nextInt();
+        }
+
+        System.out.println("최대값은 " + maxOf(height) + "입니다.");
+    }
+}
